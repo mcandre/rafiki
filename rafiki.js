8ac9919791e507439d15414aa15441428714f836
Andrew <githubmcandre.gumdrop164@passfwd.com>
2025-01-15 20:51:50 -0600

    enhance error handling

--- rafiki.js
+++ rafiki.js
@@ -6,7 +6,7 @@ const
 
 function resetIndexList() {
     indexList.empty();
-    indexList.append('<div class="index_placeholder"></div>');
+    indexList.append('<div class="index_placeholder d-print-none"></div>');
 }
 
 $('#generate').on('click', function() {
@@ -16,12 +16,11 @@ $('#generate').on('click', function() {
         requests = [],
         lines = $('#deck_list').val().split('\n'),
         errors = $('#errors'),
-        generate = $('#generate'),
-        generateSpinner = $('#generate_spinner');
+        generate = $('#generate');
     resetIndexList();
     errors.empty();
     generate.prop('disabled', !generate.prop('disabled'));
-    generateSpinner.toggle();
+    generate.text('Loading...');
 
     for (const line of lines) {
         if (line === '') {
@@ -31,14 +30,21 @@ $('#generate').on('click', function() {
         const m = EntryPattern.exec(line);
 
         if (m === null) {
-            errors.append(`<p>invalid entry: ${line}</p>`);
-            generateSpinner.toggle();
+            errors.append(`<p class="error">error: invalid entry: ${line}</p>`);
+            generate.text('Generate');
             generate.prop('disabled', !generate.prop('disabled'));
             break;
         }
 
         let [_m, _count, name] = m;
         names.push(name);
+    }
+
+    if (names.length === 0) {
+        errors.append(`<p class="warning">empty deck</p>`);
+        generate.text('Generate');
+        generate.prop('disabled', !generate.prop('disabled'));
+        return;
     }
 
     let u = new URL(LorcanaApiFetchUrl);
@@ -49,21 +55,27 @@ $('#generate').on('click', function() {
     $.get(u, function(data, status) {
         if (status < 200 || status > 299) {
             console.log(`u: ${u}`);
-            errors.append(`<p>lorcana api responded status: ${status}</p>`);
-            generateSpinner.toggle();
+            errors.append(`<p class="error">error: received lorcana api status code: ${status}</p>`);
+            generate.text('Generate');
             generate.prop('disabled', !generate.prop('disabled'));
             return;
         }
 
         if (data.length === 0) {
             console.log(`u: ${u}`);
-            errors.append(`<p>no lorcana cards match</p>`);
-            generateSpinner.toggle();
+            errors.append(`<p class="error">error: no matching card names found</p>`);
+            generate.text('Generate');
             generate.prop('disabled', !generate.prop('disabled'));
             return;
         }
 
-        console.log(`data: ${JSON.stringify(data)}`);
+        if (!Array.isArray(data)) {
+            console.log(`data: ${JSON.stringify(data)}`);
+            errors.append('<p class="error">error: lorcana api unable to process query');
+            generate.text('Generate');
+            generate.prop('disabled', !generate.prop('disabled'));
+            return;
+        }
 
         for (const d of data) {
             cardsByName[d.Name] = {
@@ -85,6 +97,16 @@ $('#generate').on('click', function() {
 
         let indices = Object.values(cardsSorted);
         indices = [...Array(Math.ceil(indices.length/Chunk))].map(_ => indices.splice(0, Chunk));
+        let deckColorsUnique = {};
+
+        for (const card of Object.values(cardsByName)) {
+            deckColorsUnique[card.color] = true;
+        }
+
+        if (Object.keys(deckColorsUnique).length > 2) {
+            errors.append('<p class="warning">warning: 3+ colors</p>');
+        }
+
         indexList.empty();
 
         for (const index of indices) {
@@ -92,7 +114,6 @@ $('#generate').on('click', function() {
 
             for (const card of index) {
                 let cardDiv = '<div class="tcg_card">';
-
                 cardDiv += `<img class="color" src="symbols/${card.color}.png" width="14" height="16" alt="${card.color}">`;
                 cardDiv += '<span class="cost ';
 
@@ -110,11 +131,12 @@ $('#generate').on('click', function() {
             indexList.append(indexDiv);
         }
 
-        generateSpinner.toggle();
+        generate.text('Generate');
         generate.prop('disabled', !generate.prop('disabled'));
     }).catch(e => {
-        errors.append(`error: ${e}`);
-        generateSpinner.toggle();
+        console.log(`u: ${u}`);
+        errors.append(`<p class="error">error: network disruption</p>`);
+        generate.text('Generate');
         generate.prop('disabled', !generate.prop('disabled'));
     });
 });
